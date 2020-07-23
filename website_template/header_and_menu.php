f6d310c44bc20aaae380e950008ed3f82fc0e739
ali awais <ali.awais5044@gmail.com>
2020-07-23 15:23:18 +0400

    news and milestones pages are added

--- website_template/header_and_menu.php
+++ website_template/header_and_menu.php
@@ -100,7 +100,7 @@
                             <ul class="nav-menu-list">
                                 <li><a href="/about/why-multibank-group.php"><?php translate('Why Multibank'); ?></a></li>
                                 <li><a href="/about/global-presence.php"><?php translate('Global Presence'); ?></a></li>
-                                <li><a href="mfx/v1/about/regulations.php"><?php translate('Regulations'); ?></a></li>
+                                <li><a href="/mfx/v1/about/regulations.php"><?php translate('Regulations'); ?></a></li>
                                 <li><a href="/about/security-of-funds.php"><?php translate('Security of Funds'); ?></a></li>
                                 <li><a href="/about/milestones.php"><?php translate('Milestones'); ?></a></li>
                                 <li><a href="/about/management.php"><?php translate('Management'); ?></a></li>
@@ -317,7 +317,7 @@
                     <ul class="text-center">
                         <li><a href="/about/why-multibank-group.php"><?php translate('Why Multibank'); ?></a></li>
                         <li><a href="/about/global-presence.php"><?php translate('Global Presence'); ?></a></li>
-                        <li><a href="mfx/v1/about/regulations.php"><?php translate('Regulations'); ?></a></li>
+                        <li><a href="/mfx/v1/about/regulations.php"><?php translate('Regulations'); ?></a></li>
                         <li><a href="/about/security-of-funds.php"><?php translate('Security of Funds'); ?></a></li>
                         <li><a href="/about/milestones.php"><?php translate('Milestones'); ?></a></li>
                         <li><a href="/about/management.php"><?php translate('Management'); ?></a></li>
