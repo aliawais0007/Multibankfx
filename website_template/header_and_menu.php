2258de38b9cc4eae8b839ab975ef96960b825ade
ali awais <ali.awais5044@gmail.com>
2020-07-23 15:22:54 +0400

    news and milestones pages are added

--- website_template/header_and_menu.php
+++ website_template/header_and_menu.php
@@ -100,14 +100,14 @@
                             <ul class="nav-menu-list">
                                 <li><a href="/about/why-multibank-group.php"><?php translate('Why Multibank'); ?></a></li>
                                 <li><a href="/about/global-presence.php"><?php translate('Global Presence'); ?></a></li>
-                                <li><a href="/mfx/v1/about/regulations.php"><?php translate('Regulations'); ?></a></li>
+                                <li><a href="mfx/v1/about/regulations.php"><?php translate('Regulations'); ?></a></li>
                                 <li><a href="/about/security-of-funds.php"><?php translate('Security of Funds'); ?></a></li>
                                 <li><a href="/about/milestones.php"><?php translate('Milestones'); ?></a></li>
                                 <li><a href="/about/management.php"><?php translate('Management'); ?></a></li>
                                 <li><a href="/about/awards.php"><?php translate('Awards') ?></a></li>
                                 <li><a href="/about/sponsorship.php"><?php translate('Sponsorship') ?></a></li>
-                                <li><a href="article/company_news.php"><?php translate('Company News'); ?></a></li>
-                                <li><a href="article/hiring-now-cyprus.php"><?php translate('Career'); ?></a></li>
+                                <li><a href="/article/company-news.php"><?php translate('Company News'); ?></a></li>
+                                <li><a href="/article/hiring-now-cyprus.php"><?php translate('Career'); ?></a></li>
                             </ul>
                         </div>
                         <div class="col-md-4 col-sm-12 col-xs-12 ">
@@ -317,13 +317,13 @@
                     <ul class="text-center">
                         <li><a href="/about/why-multibank-group.php"><?php translate('Why Multibank'); ?></a></li>
                         <li><a href="/about/global-presence.php"><?php translate('Global Presence'); ?></a></li>
-                        <li><a href="//mfx/v1/about/regulations.php"><?php translate('Regulations'); ?></a></li>
+                        <li><a href="mfx/v1/about/regulations.php"><?php translate('Regulations'); ?></a></li>
                         <li><a href="/about/security-of-funds.php"><?php translate('Security of Funds'); ?></a></li>
                         <li><a href="/about/milestones.php"><?php translate('Milestones'); ?></a></li>
                         <li><a href="/about/management.php"><?php translate('Management'); ?></a></li>
                         <li><a href="/about/awards.php"><?php translate('Awards'); ?></a></li>
                         <li><a href="/about/sponsorship.php"><?php translate('Sponsorship'); ?></a></li>
-                        <li><a href="/article/company-news.php"><?php translate('Company News'); ?></a></li>
+                        <li><a href="/article/news_20180916.php"><?php translate('Company News'); ?></a></li>
                         <li><a href="/article/hiring-now-cyprus.php"><?php translate('Career'); ?></a></li>
                     </ul>
                 </li>
