be7407e5f17972246ba1be2cfd6b5230f41a6468
ali awais <ali.awais5044@gmail.com>
2020-07-23 17:49:50 +0400

    news and milestones pages are added

--- database/db_driver.php
+++ database/db_driver.php
@@ -75,13 +75,13 @@ function translate($text,$page_name=''){
 }
 
 function get_news(){
-	$dbConnection1 = new PDO('sqlite:/var/www/multibankfx/database/db.sqlite3',"","",array(
-		PDO::ATTR_PERSISTENT => TRUE,
-		PDO::ERRMODE_EXCEPTION => TRUE));
-		$stmt = $dbConnection1->query("SELECT * FROM company_news");
-		while ($row = $stmt->fetch()) {
-			echo "ali".$row['title']."<br />\n";
-		}
+	// $dbConnection1 = new PDO('sqlite:/var/www/multibankfx/database/db.sqlite3',"","",array(
+	// 	PDO::ATTR_PERSISTENT => TRUE,
+	// 	PDO::ERRMODE_EXCEPTION => TRUE));
+	// 	$stmt = $dbConnection1->query("SELECT * FROM company_news");
+	// 	while ($row = $stmt->fetch()) {
+	// 		echo "ali".$row['title']."<br />\n";
+	// 	}
 		echo "ali"."<br />\n";
 }
 
