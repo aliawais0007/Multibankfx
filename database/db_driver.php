fe8fd7929bde711c09ffd719f5d2fd0cca364114
ali awais <ali.awais5044@gmail.com>
2020-07-26 16:56:34 +0400

    fixed next previous button in news

--- database/db_driver.php
+++ database/db_driver.php
@@ -164,8 +164,8 @@ function filtered_milestone(){
         $object = new stdClass();
         $object->id=$a[0];
         $object->title=$a[1];
-        $object->main_title=$a[5];
-        $object->featured_image=$a[4];
+        $object->main_title=$a[4];
+        $object->featured_image=$a[5];
         $object->body=$a[3];
         $object->date=$a[2];
         return $object;       
