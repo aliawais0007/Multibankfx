f84b3392cfb5cc8f3a4b82b04bcf0d97d823f753
ali awais <ali.awais5044@gmail.com>
2020-07-26 04:19:49 +0400

    fixed next previous button in news

--- article/company-news.php
+++ article/company-news.php
@@ -4,7 +4,10 @@ $pagNews = array_slice($allnews, 0,7);
 $itemc = "";
 $item = "";
 $itemb = "";
-$page = isset($_GET['id']) ? $_GET['id'] :1;
+$page = "";
+if(isset($_GET['id'])){
+    $page =$_GET['id'];
+} else $page = '1';
     
 
     foreach ($pagNews as $key=>$list) {
@@ -1807,14 +1810,14 @@ $page = isset($_GET['id']) ? $_GET['id'] :1;
             <div class="col-12 col-lg-9 col-lg-offset-1 mex-m-b-50 text-center">
                 <nav aria-label="Page navigation example">
                     <ul class="pagination justify-content-center">
-                        <li class="page-item disabled"><a class="page-link" href="/article/company-news.php<?php echo '?page='.$page-1?>" tabindex="-1">Previous</a></li>
+                        <li class="page-item disabled"><a class="page-link" href="/article/company-news.php?page=<?php echo $page-1?>" tabindex="-1">Previous</a></li>
                         <?php 
                         for($i=1; $i<=7; $i++){
                             echo "<li class='page-item active'><a class='page-link' href='/article/company-news.php?page=$i'>$i</a></li>";
                         }
                         
                         ?>
-                        <li class="page-item "><a class="page-link" href="/article/company-news.php<?php echo '?page='.$page+1;?>">Next</a>
+                        <li class="page-item "><a class="page-link" href="/article/company-news.php?page=<?php echo $page+1;?>">Next</a>
                         </li>
                     </ul>
                 </nav>
