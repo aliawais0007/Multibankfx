29c6f8f92eb5df464496f202008bcc10ae7a5eee
ali awais <ali.awais5044@gmail.com>
2020-07-27 11:16:47 +0400

    contact us form

--- database/db_driver.php
+++ database/db_driver.php
@@ -146,7 +146,7 @@ function filtered_news(){
         $object->featured_image=$a[4];
         $object->body=$a[3];
         $object->date=$a[1];
-        echo $object;
+        print_r($object);
         return $object;     
 }
 
