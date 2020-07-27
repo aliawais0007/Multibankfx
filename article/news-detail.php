e48e3f1a82a59defc6ea54755c993d90e1fae0d8
ali awais <ali.awais5044@gmail.com>
2020-07-27 11:53:51 +0400

    contact us form

--- article/news-detail.php
+++ article/news-detail.php
@@ -76,6 +76,7 @@ $news = filtered_news();
                         <img src="<?php echo $news->featured_image?$news->featured_image:$news->post_image?>" class="article-img" />
                         <?php echo $news->body?>
                     </div>
+                    </div>
                     <style>
                         .grapesjs-editable-field-item[data-grapesjs-field="field_component_01"][data-grapesjs-delta="0"] * {
                             box-sizing: border-box;
