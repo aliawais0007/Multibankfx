8c4248a112d2bd0a2a507410e49bd96e478b9602
ali awais <ali.awais5044@gmail.com>
2020-08-03 11:50:30 +0400

    forms changed backend

--- forms/forms.php
+++ forms/forms.php
@@ -21,64 +21,64 @@ if ($_POST) {
     $status = 1;
 
     print_r($_POST);
-    if (isset($_POST['footer_form'])) {
+    // if (isset($_POST['footer_form'])) {
 
-        //footer from
-        $STH = $dbConnection->prepare("INSERT INTO myadmin_call_back (name,email,phone,status) VALUES ('$name', '$email','$phone','$status')");
-        $result =$STH->execute();
-        echo $result;
-        echo "inerted data";
-        //........
-    }
-    if (isset($_POST['partner_form'])) {
+    //     //footer from
+    //     $STH = $dbConnection->prepare("INSERT INTO myadmin_call_back (name,email,phone,status) VALUES ('$name', '$email','$phone','$status')");
+    //     $result =$STH->execute();
+    //     echo $result;
+    //     echo "inerted data";
+    //     //........
+    // }
+    // if (isset($_POST['partner_form'])) {
         //........
         //partner from
 
-        $query = "INSERT INTO myadmin_partner (name,email,phone,status) VALUES ('$name', '$email','$phone','$status";
-        $STH = $dbConnection->prepare($query);
-        $result =$STH->execute();
-        echo $result;
-    }
-    if (isset($_POST['account_type'])) {
+    //     $query = "INSERT INTO myadmin_partner (name,email,phone,status) VALUES ('$name', '$email','$phone','$status";
+    //     $STH = $dbConnection->prepare($query);
+    //     $result =$STH->execute();
+    //     echo $result;
+    // }
+    // if (isset($_POST['account_type'])) {
 
 
-        //contact us form
-        $query = "INSERT INTO myadmin_contact_us (status,fullname,email,phone,contacttype) VALUES ('$status','$name', '$email','$phone','$contact_type')";
-        $STH = $dbConnection->prepare($query);
-        $result =$STH->execute();
-        echo $result;
-    }
-    if (isset($_POST['platform_version'])) {
+    //     //contact us form
+    //     $query = "INSERT INTO myadmin_contact_us (status,fullname,email,phone,contacttype) VALUES ('$status','$name', '$email','$phone','$contact_type')";
+    //     $STH = $dbConnection->prepare($query);
+    //     $result =$STH->execute();
+    //     echo $result;
+    // }
+    // if (isset($_POST['platform_version'])) {
         //demo from
-        $db->exec("INSERT INTO myadmin_demo_account (name,mobile,email,platform) VALUES ('$name', '$email','$phone','$platform')");
-        echo "inerted data";
-    }
+    //     $db->exec("INSERT INTO myadmin_demo_account (name,mobile,email,platform) VALUES ('$name', '$email','$phone','$platform')");
+    //     echo "inerted data";
+    // }
 
-    if (isset($_POST['company'])) {
+    // if (isset($_POST['company'])) {
 
         //
-        $target_file = "";
-        $target_dir = "uploads/";
+        // $target_file = "";
+        // $target_dir = "uploads/";
 
-        $image = $_FILES['filehi']['name'];
-        $target_file = $target_dir . basename($_FILES["filehi"]["name"]);
-        $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-        $imagepath = "";
+        // $image = $_FILES['filehi']['name'];
+        // $target_file = $target_dir . basename($_FILES["filehi"]["name"]);
+        // $uploadOk = 1;
+        // $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        // $imagepath = "";
 
-        if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.";
+        // if ($uploadOk == 0) {
+        //     echo "Sorry, your file was not uploaded.";
             // if everything is ok, try to upload file
-        } else {
-            if (move_uploaded_file($_FILES["filehi"]["tmp_name"], $target_file)) {
+        // } else {
+        //     if (move_uploaded_file($_FILES["filehi"]["tmp_name"], $target_file)) {
                 // echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-                $imagepath = basename($_FILES["filehi"]["name"]);
-            } else {
-                echo "Sorry, there was an error uploading your file.";
-            }
-        }
+    //             $imagepath = basename($_FILES["filehi"]["name"]);
+    //         } else {
+    //             echo "Sorry, there was an error uploading your file.";
+    //         }
+    //     }
 
-        $db->exec("INSERTINTO myadmin_live_account (company, platform_version, platform_type, leverage, client_type, lei_code, has_tin, tin_number, name, name2, email, phone_display, image)
-       VALUES ( '$company', '$platform', '$platform_type', '$leverage', '$client_type', '$lei_code', '$has_tin', '$tin_number', '$name', '$name2', '$email', '$phone', '$imagepath');");
-    }
+    //     $db->exec("INSERTINTO myadmin_live_account (company, platform_version, platform_type, leverage, client_type, lei_code, has_tin, tin_number, name, name2, email, phone_display, image)
+    //    VALUES ( '$company', '$platform', '$platform_type', '$leverage', '$client_type', '$lei_code', '$has_tin', '$tin_number', '$name', '$name2', '$email', '$phone', '$imagepath');");
+    // }
 }
