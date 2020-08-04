d8afc43f1632bfe44fe561bbb0bc23ef4b8515c5
ali awais <ali.awais5044@gmail.com>
2020-08-04 17:39:52 +0400

    fixes

--- social-trading.php
+++ social-trading.php
@@ -902,7 +902,7 @@
 
         }
     </style>
-    <script data-cfasync="false" src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
     <script type="bdaa9b02232eaa47ac948bf3-text/javascript">
         /*$(function() {
     $('.nav-link').click(function() {
