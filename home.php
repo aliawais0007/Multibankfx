a7bb50c6b1289743a0c5e4e7ba7e9dc5e6b4afa8
ali awais <ali.awais5044@gmail.com>
2020-08-05 17:51:08 +0400

    fixes

--- home.php
+++ home.php
@@ -1437,7 +1437,7 @@ foreach ($allnews as $item) {
     <style type="text/css">
         .slidebar a span.btn {
             padding: 0;
-            background: none !important;
+            background: none ;
             margin: 0 0 3px 0;
         }
 
