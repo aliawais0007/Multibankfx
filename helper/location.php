ff28a3da9b2c15b9164ed079d3a1cb2655fdccb9
ali awais <ali.awais5044@gmail.com>
2020-08-08 18:02:39 +0400

    location

--- helper/location.php
+++ helper/location.php
@@ -1,12 +1,12 @@
 <?php 
 $ip =  $_SERVER['REMOTE_ADDR'];
 $url = "http://api.hostip.info/get_html.php?ip=$ip";
-echo $url;
+// echo $url;
 $location = file_get_contents('http://freegeoip.net/json/'.$_SERVER['REMOTE_ADDR']);
 echo $location;
-echo $country;
+// echo $country;
 // Reformat the data returned (Keep only country and country abbr.)
 $only_country=explode (" ", $country);
 
-echo "Country : ".$only_country[1]." ".substr($only_country[2],0,4);
+// echo "Country : ".$only_country[1]." ".substr($only_country[2],0,4);
 ?>
