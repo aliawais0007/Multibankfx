8902c932d12b3e50da82f6ca32f5ceae2b376fff
ali awais <ali.awais5044@gmail.com>
2020-08-08 18:18:22 +0400

    location

--- helper/location.php
+++ helper/location.php
@@ -30,7 +30,7 @@ print_r($location);
 $.get("https://ipinfo.io/json", function (response) {
     $("#ip").html("IP: " + response.ip);
     $("#address").html("Location: " + response.city + ", " + response.region);
-    $("#details").html(JSON.stringify(response, null, 4));
+    $("#details").html(JSON.stringify(response));
 }, "jsonp");
 </script>
 </body>
--- website_template/page_footer.php
+++ website_template/page_footer.php
@@ -121,3 +121,10 @@
     </div>
 </section>
 
+<script>
+$.get("https://ipinfo.io/json", function (response) {
+    $("#ip").html("IP: " + response.ip);
+    $("#address").html("Location: " + response.city + ", " + response.region);
+    $("#details").html(JSON.stringify(response));
+}, "jsonp");
+</script>
