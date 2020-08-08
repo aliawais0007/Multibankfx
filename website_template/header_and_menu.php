c1e50e44d8943478f13da1fd8a5d6f9c0f2fa0cd
ali awais <ali.awais5044@gmail.com>
2020-08-08 23:39:58 +0400

    location

--- website_template/header_and_menu.php
+++ website_template/header_and_menu.php
@@ -280,10 +280,10 @@
                 </li>
             </ul>
             <ul class="nav-menu-mobile text-uppercase">
-                <li style="margin-left:1.5rem !important">
+                <li style="margin:1rem">
                     <a>
                         <div class="theme-switch-wrapper d-flex justify-content-between">
-                            <em id="darkmode-text">Enable Dark Mode!</em>
+                            <span id="darkmode-text">Enable Dark Mode!</span>
                             <label class="theme-switch" for="checkbox">
                                 <input type="checkbox" id="checkbox"/>
                                 <div class="slider round"></div>
