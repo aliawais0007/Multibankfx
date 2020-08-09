2f8227b7a2874c6b4ce4223e7fce7f4a74e2ad44
ali awais <ali.awais5044@gmail.com>
2020-08-09 10:14:24 +0400

    fixes

--- partnership/cpa-affiliate-program.php
+++ partnership/cpa-affiliate-program.php
@@ -433,7 +433,6 @@
         <style type="text/css">
             .slidebar a span.btn {
                 padding: 0;
-                background: none !important;
                 margin: 0 0 3px 0;
             }
 
