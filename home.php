0a46fcd4c4dad9b785d68d72d71a455bd0eac18d
ali awais <ali.awais5044@gmail.com>
2020-08-10 18:08:56 +0400

    fixes

--- home.php
+++ home.php
@@ -2223,8 +2223,7 @@ foreach ($allnews as $item) {
             // banner slider under Hero slider
             $('#home-carousal-banner').slick({
                 centerMode: true,
-                centerPadding: '10px',
-                slidesToShow: 1,
+                slidesToShow: 2,
                 infinite: false,
                 arrows: false,
             });
