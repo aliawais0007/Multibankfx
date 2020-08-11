acdf3207a3bc3d809f545f715d11f0df6f7a2aef
ali awais <ali.awais5044@gmail.com>
2020-08-11 18:03:04 +0400

    fixes

--- website_template/head.php
+++ website_template/head.php
@@ -7,17 +7,16 @@
     <meta name='keywords' content="live account forms, pdf trade account application forms, forex pdf application, individual trade account form, joint trading account, corporate trading account application, forex broker transfer form, VPS account application form, request changes for fore">
     <meta property='og:title' content="Client Forms | Download Account Application Forms">
     <meta property='og:description' content="Access and download our Account Forms, including Client Agreement, Financial Services Guide, Product Disclosure Statement, Account Application Forms.">
-    <link rel='canonical' href="/account-forms.php">
-    <link rel="alternate" href="https://es.multibankfx.com/account/account-forms" hreflang="es-ES" />
-    <link rel="alternate" href="https://ru.multibankfx.com/account/account-forms" hreflang="ru-RU" />
-    <link rel="alternate" href="https://ae.multibankfx.com/account/account-forms" hreflang="en-ae" />
-    <link rel="alternate" href="https://vn.multibankfx.com/account/account-forms" hreflang="vi-VN" />
-    <link rel="alternate" href="https://id.multibankfx.com/account/account-forms" hreflang="id-ID" />
-    <link rel="alternate" href="https://mys.multibankfx.com/account/account-forms" hreflang="ms-MY" />
-    <link rel="alternate" href="https://ph.multibankfx.com/account/account-forms" hreflang="tl-PH" />
-    <link rel="alternate" href="https://th.multibankfx.com/account/account-forms" hreflang="th-TH" />
-    <link rel="alternate" href="https://fr.multibankfx.com/account/account-forms" hreflang="fr-FR" />
-    <link rel="alternate" href="/account-forms.php" hreflang="x-default" />
+    <link rel='canonical' href="https://multibankfx.com">
+    <link rel="alternate" href="https://multibankfx.com/en" hreflang="x-default" />
+    <link rel="alternate" href="https://multibankfx.com/es" hreflang="es" />
+    <link rel="alternate" href="https://multibankfx.com/ru" hreflang="ru" />
+    <link rel="alternate" href="https://multibankfx.com/ar" hreflang="ar" />
+    <link rel="alternate" href="https://multibankfx.com/vn" hreflang="vn" />
+    <link rel="alternate" href="https://multibankfx.com/id" hreflang="id" />
+    <link rel="alternate" href="https://multibankfx.com/ms" hreflang="ms" />
+    <link rel="alternate" href="https://multibankfx.com/th" hreflang="th" />
+    <link rel="alternate" href="https://multibankfx.com/fr" hreflang="fr" />
 
     <script type="text/javascript" async>
         function init_google(w, d, s, l, i) {
