6449842922cc56213b76eddb0d65026d5de6884a
ali awais <ali.awais5044@gmail.com>
2020-08-11 10:49:37 +0400

    fixes

--- website_template/head.php
+++ website_template/head.php
@@ -90,7 +90,7 @@
     <link rel="stylesheet" type="text/css" href="/public_files/static_css/swiper-3.4.2.min.css">
     <link rel="stylesheet" href="/public_files/static_css/rtl.css">
     <link rel="stylesheet" href="/public_files/static_css/rtl.css">
-<link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"crossorigin="anonymous">
+<!-- <link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"crossorigin="anonymous"> -->
 
 
 
