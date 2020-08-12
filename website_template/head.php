569b047203c1d51931b04e59a99945d42020f6b6
ali awais <ali.awais5044@gmail.com>
2020-08-12 13:33:35 +0400

    fixes

--- website_template/head.php
+++ website_template/head.php
@@ -18,28 +18,8 @@
     <link rel="alternate" href="https://multibankfx.com/th" hreflang="th" />
     <link rel="alternate" href="https://multibankfx.com/fr" hreflang="fr" />
 
-    <script type="text/javascript" async>
-        function init_google(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        }
-        setTimeout(function() {
-            init_google(window, document, 'script', 'dataLayer', 'GTM-KNJQJTN')
-        }, 10000)
-    </script>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link rel="shortcut icon" href="/public_files/images/common/icons/favicon.ico">
-    <!-- <link rel="stylesheet" href="/public_files/static_min_css/library.min.css"> -->
+    <link rel="stylesheet" href="/public_files/static_min_css/library.min.css">
     <!-- <link rel="stylesheet" href="/public_files/static_min_css/custom.min.css"> -->
     <link rel="stylesheet" href="/public_files/static_css/blackmode.css">
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
--- website_template/page_footer.php
+++ website_template/page_footer.php
@@ -215,12 +215,24 @@
     
 </script>
 <script id="et-iframe" data-version="0.5" data-widgetId="5f33b2fac026597522455eb1" src="https://client.consolto.com/iframeApp/iframeApp.js"  ></script>
-     <!-- Start of REVE Chat Script-->
- <!-- <script type='text/javascript'>
- window.$_REVECHAT_API || (function(d, w) { var r = $_REVECHAT_API = function(c) {r._.push(c);}; w.__revechat_account='6516159';w.__revechat_version=2;
-   r._= []; var rc = d.createElement('script'); rc.type = 'text/javascript'; rc.async = true; rc.setAttribute('charset', 'utf-8');
-   rc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'static.revechat.com/widget/scripts/new-livechat.js?'+new Date().getTime();
-   var s = d.getElementsByTagName('script')[0]; s.parentNode.insertBefore(rc, s);
- })(document, window);
-</script> -->
-<!-- End of REVE Chat Script -->
+
+    <script type="text/javascript" async>
+        function init_google(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        }
+        setTimeout(function() {
+            init_google(window, document, 'script', 'dataLayer', 'GTM-KNJQJTN')
+        }, 10000)
+    </script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
