29c969868fc495b0e448bf1ea5b7a3c4b8900772
ali awais <ali.awais5044@gmail.com>
2020-08-12 14:47:03 +0400

    fixes

--- website_template/page_footer.php
+++ website_template/page_footer.php
@@ -235,7 +235,7 @@
 </script>
  <!-- Start of REVE Chat Script-->
  <script type='text/javascript'>
- window.$_REVECHAT_API || (function(d, w) { var r = $_REVECHAT_API = function(c) {r._.push(c);}; w.__revechat_account='6516159';w.__revechat_version=2;
+ window.$_REVECHAT_API || (function(d, w) { var r = $_REVECHAT_API = function(c) {r._.push(c);}; w.__revechat_account='6598532';w.__revechat_version=2;
    r._= []; var rc = d.createElement('script'); rc.type = 'text/javascript'; rc.async = true; rc.setAttribute('charset', 'utf-8');
    rc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'static.revechat.com/widget/scripts/new-livechat.js?'+new Date().getTime();
    var s = d.getElementsByTagName('script')[0]; s.parentNode.insertBefore(rc, s);
