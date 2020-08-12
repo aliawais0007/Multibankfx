4e3470c4e66b34da0688341f2796a9e4aa0ba506
ali awais <ali.awais5044@gmail.com>
2020-08-12 13:31:53 +0400

    fixes

--- website_template/head.php
+++ website_template/head.php
@@ -39,7 +39,7 @@
     </script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link rel="shortcut icon" href="/public_files/images/common/icons/favicon.ico">
-    <link rel="stylesheet" href="/public_files/static_min_css/library.min.css">
+    <!-- <link rel="stylesheet" href="/public_files/static_min_css/library.min.css"> -->
     <!-- <link rel="stylesheet" href="/public_files/static_min_css/custom.min.css"> -->
     <link rel="stylesheet" href="/public_files/static_css/blackmode.css">
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
